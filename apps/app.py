a08144c93c49566b0a20b8bbb44436103e3500e9
kyomaru kikuchi <kyomaru1543@gmail.com>
2025-10-22 20:13:34 +0900

    deploy6

--- apps/app.py
+++ apps/app.py
@@ -23,3 +23,7 @@ def create_app(config_key):
     # app.register_blueprint(data_views.data, url_prefix="/data")
 
     return app
+
+
+if __name__ == "__main__":
+    create_app.run(host="0.0.0.0", port=5000, debug=True)
